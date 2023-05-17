1fe0f109a3ac6c1e4ece0a31b67fecac5557801b
Bombo <bombo96@chungwoon.ac.kr>
2023-05-17 15:36:14 +0900

    판매원과 판매점 의존도 줄이기 -> 결합도 낮추기

--- chapter1/src/main/java/object/chapter1/domain/Theater.java
+++ chapter1/src/main/java/object/chapter1/domain/Theater.java
@@ -11,15 +11,6 @@ public class Theater {
     }
 
     public void enter(Audience audience) {
-        if(audience.getBag().hasInvitation()) { // 초대권을 소지하고 있는 경우
-            // 이미 가지고 있는 티켓과 초대권을 교환한다.
-            Ticket ticket = ticketSeller.getTicketOffice().getTicket();
-            audience.getBag().setTicket(ticket);
-        } else { // 고객은 티켓을 구매하여 소지한다. 이때, 돈 거래가 이루어진다.
-            Ticket ticket = ticketSeller.getTicketOffice().getTicket();
-            audience.getBag().minusAmount(ticket.getFee());
-            ticketSeller.getTicketOffice().plusAmount(ticket.getFee());
-            audience.getBag().setTicket(ticket);
-        }
+        ticketSeller.sellTo(audience);
     }
 }
