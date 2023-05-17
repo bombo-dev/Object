9fdd5799264f18f4248dfe9f7ee1617fb43fd9b5
Bombo <bombo96@chungwoon.ac.kr>
2023-05-17 15:39:13 +0900

    고객에 대한 행위 명시 및 결합도 낮추기

--- chapter1/src/main/java/object/chapter1/domain/Audience.java
+++ chapter1/src/main/java/object/chapter1/domain/Audience.java
@@ -1,8 +1,5 @@
 package object.chapter1.domain;
 
-import lombok.Data;
-
-@Data
 public class Audience {
     private Bag bag;
 
@@ -10,7 +7,14 @@ public class Audience {
         this.bag = bag;
     }
 
-    public Bag getBag() {
-        return bag;
+    public Long buy(Ticket ticket) {
+        if (bag.hasInvitation()) {
+            bag.setTicket(ticket);
+            return 0L;
+        } else {
+            bag.setTicket(ticket);
+            bag.minusAmount(ticket.getFee());
+            return ticket.getFee();
+        }
     }
 }
